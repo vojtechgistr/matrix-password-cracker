96a715421bb4f66c8961fb7db41cfb9d2b56920b
Vojtěch Gistr (AdalberToo) <vojtech.gistr@gmail.com>
2024-04-16 12:01:01 +0200

    Add files via upload

---
+++ src/MatrixPasswordCracker/MatrixRainHandler.cs
@@ -0,0 +1,74 @@
+﻿namespace MatrixRain
+{
+    class MatrixRainHandler
+    {
+        Random rand = new Random();
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int[] Y { get; set; }
+
+        public MatrixRainHandler()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WindowLeft = Console.WindowTop = 0;
+            Console.WindowHeight = Console.BufferHeight = Console.LargestWindowHeight;
+            Console.WindowWidth = Console.BufferWidth = Console.LargestWindowWidth;
+            Console.CursorVisible = false;
+
+            int width, height;
+            int[] y;
+
+            Initialize(out width, out height, out y);
+            Width = width;
+            Height = height;
+            Y = y;
+        }
+
+
+
+        public void UpdateAllColumns(int width, int height, int[] y, string character)
+        {
+            int x;
+            for (x = 0; x < width; ++x)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.SetCursorPosition(x, y[x]);
+                Console.Write(character);
+                
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                int temp = y[x] - 2;
+                Console.SetCursorPosition(x, inScreenYPosition(temp, height));
+                Console.Write(character);
+
+                int temp1 = y[x] - 20;
+                Console.SetCursorPosition(x, inScreenYPosition(temp1, height));
+                Console.Write(' ');
+
+                y[x] = inScreenYPosition(y[x] + 1, height);
+            }
+        }
+
+        public int inScreenYPosition(int yPosition, int height)
+        {
+            if (yPosition < 0)
+                return yPosition + height;
+            else if (yPosition < height)
+                return yPosition;
+            else
+                return 0;
+        }
+
+        private void Initialize(out int width, out int height, out int[] y)
+        {
+            height = Console.WindowHeight;
+            width = Console.WindowWidth - 1;
+            y = new int[width];
+            Console.Clear();
+
+            for (int x = 0; x < width; ++x)
+            {
+                y[x] = rand.Next(height);
+            }
+        }
+    }
+}
---
+++ src/MatrixPasswordCracker/Program.cs
@@ -0,0 +1,69 @@
+﻿using MatrixRain;
+
+namespace MatrixPasswordCracker
+{
+    internal class Program
+    {
+        static char[] Match =            {'0','c','d','e','f','g','1','2','3','4','5','a','b','h','i','6','7','8','9','j' ,'k','l','m','n','o','p',
+                        'q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','C','L','M','N','O','P',
+                        'Q','R','S','T','U','V','X','Y','Z','!','?',' ','*','-','+'};
+        
+        static string FindPassword;
+        static int Combi;
+        static string space;
+        static int Characters;
+        private static MatrixRainHandler _program;
+
+        static void Main(string[] args)
+        {
+            _program = new MatrixRainHandler();
+            Console.Title = "Matrix Password Cracker";
+            Console.ForegroundColor = ConsoleColor.White;
+            int Count;
+            space = " ";
+
+            Console.WriteLine("Enter your Password:");
+            FindPassword = Console.ReadLine();
+            Characters = FindPassword.Length;
+
+            Console.Clear();
+
+            DateTime today = DateTime.Now;
+            today.ToString("yyyy-MM-dd_HH:mm:ss");
+            Console.WriteLine(space);
+            Console.WriteLine("START:\t{0}", today);
+
+            for (Count = 0; Count <= 15; Count++)
+            {
+                Recurse(Count, 0, "");
+            }
+        }
+
+        static void Recurse(int Lenght, int Position, string BaseString)
+        {
+            for (int Count = 0; Count < Match.Length; Count++)
+            {
+                Combi++;
+                if (Position < Lenght - 1)
+                {
+                    _program.UpdateAllColumns(_program.Width, _program.Height, _program.Y, BaseString);
+                    Recurse(Lenght, Position + 1, BaseString + Match[Count]);
+                }
+                if (BaseString + Match[Count] == FindPassword)
+                {
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Your password is: " + FindPassword);
+                    Console.WriteLine("Your password is: " + Characters + " character(s) long");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    DateTime today = DateTime.Now;
+                    today.ToString("yyyy-MM-dd_HH:mm:ss");
+                    Console.WriteLine(space);
+                    Console.WriteLine("END:\t{0}\nCombi:\t{1}", today, Combi);
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
